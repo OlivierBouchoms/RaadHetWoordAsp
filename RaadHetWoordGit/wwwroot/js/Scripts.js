846039aa652f3fdd523bcd11a088f5ab9aaf7be5
Olivier <olivier_2000@live.nl>
2018-04-10 11:02:29 +0200

    Javascript refactoring

--- RaadHetWoordGit/wwwroot/js/Scripts.js
+++ RaadHetWoordGit/wwwroot/js/Scripts.js
@@ -16,19 +16,7 @@ var $increase;
  * In the callback, we update the DOM (Document Object Model) with the product information.
  */
 
-$(document).ready(function getProducts() {
-    //Data ophalen
-    $.getJSON(uri)
-        .done(function (data) {
-            $.each(data, function (key, item) {
-                $('<li>', { text: formatItem(item) }).appendTo($('#products'));
-            });
-        });
-});
-
-function formatItem(item) {
-    return item.name + ': \u20AC ' + item.sales + ' (id = ' + item.id + ')';
-}
+$(document).ready(getProducts() );
 
 //Send a request to insert a product
 function insertProduct() {
@@ -37,19 +25,11 @@ function insertProduct() {
       
     $.ajax({
         type: 'POST',
-        url: 'api/product',
+        url: uri,
         data: { name: $name, sales: $sales },  
         dataType: "json",
         success: function () {
-            //Unsorted list leegmaken
-            $('#products').empty();
-            //Data ophalen
-            $.getJSON(uri)
-                .done(function (data) {
-                    $.each(data, function (key, item) {
-                        $('<li>', { text: formatItem(item) }).appendTo($('#products'));
-                    });
-                });
+            getProducts();
         },
         error: function () { alert('Is foutgegaan'); }
         });
@@ -60,19 +40,11 @@ function deleteProduct() {
     $id = $("#productID").val();
     $.ajax({
         type: 'DELETE',
-        url: 'api/product',
+        url: uri,
         data: { id: $id },
         dataType: "json",
-        success: function () {
-            //Unsorted list leegmaken
-            $('#products').empty();
-            //Data ophalen
-            $.getJSON(uri)
-                .done(function (data) {
-                    $.each(data, function (key, item) {
-                        $('<li>', { text: formatItem(item) }).appendTo($('#products'));
-                    });
-                });
+        success: function() {
+            getProducts();
         },
         error: function () { alert('Is foutgegaan'); }
     });
@@ -87,29 +59,39 @@ function changeProductPrice() {
     }
     $.ajax({
         type: 'PATCH',
-        url: 'api/product',
+        url: uri,
         data: { id: $id, increase: $increase },
         dataType: "json",
         success: function () {
-            //Unsorted list leegmaken
-            $('#products').empty();
-            //Data ophalen
-            $.getJSON(uri)
-                .done(function (data) {
-                    $.each(data, function (key, item) {
-                        $('<li>', { text: formatItem(item) }).appendTo($('#products'));
-                    });
-                });
-            if ($("#button1").hasClass("incorrect")) {
-                $increase = true;
-                $('#button1').removeClass();
-                $('#button1').addClass("iscorrect--");
-            }
-            else {
-                $('#button1').removeClass();
-                $('#button1').addClass("incorrect");
-            }
+            getProducts();
+            changeButtonClass();
         },
         error: function () { alert('Is foutgegaan'); }
     });
+}
+
+function changeButtonClass() {
+    if ($("#button1").hasClass("incorrect")) {
+        $increase = true;
+        $('#button1').removeClass();
+        $('#button1').addClass("iscorrect--");
+    }
+    else {
+        $('#button1').removeClass();
+        $('#button1').addClass("incorrect");
+    }
+}
+
+function getProducts() {
+    $('#products').empty();
+    $.getJSON(uri)
+        .done(function (data) {
+            $.each(data, function (key, item) {
+                $('<li>', { text: formatItem(item) }).appendTo($('#products'));
+            });
+        });
+}
+
+function formatItem(item) {
+    return item.name + ': \u20AC ' + item.sales + ' (id = ' + item.id + ')';
 }
