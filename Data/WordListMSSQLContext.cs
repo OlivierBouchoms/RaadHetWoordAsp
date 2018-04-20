6f527766905a5f739b20008a4267285620080f51
Olivier <olivier_2000@live.nl>
2018-04-20 09:42:36 +0200

    Close and dispose all connections in WordlistMSSQL

--- Data/WordListMSSQLContext.cs
+++ Data/WordListMSSQLContext.cs
@@ -27,11 +27,14 @@ namespace Data
                 words.Add(sqlDataReader.GetString(0));
             }
 
+            sqlDataReader.Close();
+            sqlDataReader.Dispose();
             sqlConnection.Close();
+            sqlConnection.Dispose();
             return words;
         }
 
-        public List<string> GetWordsFromWordlist()
+        public List<string> GetWordsFromWordlist(int id)
         {
             var words = new List<string>();
             var sqlConnection = DataBase.MsSql;
@@ -45,9 +48,10 @@ namespace Data
             }
 
             string query = "SELECT [idWord], [text] FROM Word where [idWord] IN " +
-                           "(SELECT [WordID] FROM [Word_and_WordCategorie] where [WordCategoryID]='0')";
+                           "(SELECT [WordID] FROM [Word_and_WordCategorie] where [WordCategoryID]=@id)";
             var dataTable = new DataTable();
             var sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.Add("id", SqlDbType.Int).Value = id;
             var sqlDataAdapter = new SqlDataAdapter(sqlCommand);
             sqlDataAdapter.Fill(dataTable);
             foreach (DataRow row in dataTable.Rows)
@@ -56,7 +60,7 @@ namespace Data
             }
 
             sqlConnection.Close();
-            sqlConnection = null;
+            sqlConnection.Dispose();
             return words;
         }
     }
