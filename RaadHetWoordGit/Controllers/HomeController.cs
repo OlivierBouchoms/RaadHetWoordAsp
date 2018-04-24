6688a946a28134dcd6737abe5791e1e2cc9e7277
Olivier <olivier_2000@live.nl>
2018-04-24 10:07:01 +0200

    Remove testmethod

--- RaadHetWoordGit/Controllers/HomeController.cs
+++ RaadHetWoordGit/Controllers/HomeController.cs
@@ -11,20 +11,7 @@ namespace RaadHetWoordGit.Controllers
         public IActionResult Index()
         {
             HttpContext.Session.Clear();
-            ThrowException();
             return View();
-        }
-
-        private void ThrowException()
-        {
-            try
-            {
-                throw new ArgumentNullException();
-            }
-            catch (Exception e)
-            {
-                new ExceptionLogLogic(new ExceptionLogRepository(new ExceptionXMLContext())).LogException(e);
-            }
         }
     }
 }
