de3d37df39a8e25198ed5b643d62ad02ea80aae5
Olivier <olivier_2000@live.nl>
2018-04-25 10:55:05 +0200

    Remove unused fields

--- RaadHetWoordGit/Controllers/LeaderboardsController.cs
+++ RaadHetWoordGit/Controllers/LeaderboardsController.cs
@@ -12,9 +12,6 @@ namespace RaadHetWoordGit.Controllers
     public class LeaderboardsController : Controller
     {
         private TeamLogic _teamLogic;
-        private TeamInGameLogic _teamInGameLogic;
-        private GameLogic _gameLogic;
-        private WordListLogic _wordListLogic;
        
         public IActionResult Index()
         {
@@ -35,10 +32,7 @@ namespace RaadHetWoordGit.Controllers
         /// </summary>
         private void InitializeLogic()
         {
-            _gameLogic = new GameLogic(new GameRepository(new GameMemoryContext()));
             _teamLogic = new TeamLogic(new TeamRepository(new TeamMSSQLContext()));
-            _teamInGameLogic = new TeamInGameLogic(new TeamInGameRepository(new TeamInGameMemoryContext()));
-            _wordListLogic = new WordListLogic(new WordListRepository(new WordListMSSQLContext()));
         }
     }
 }
