412da4d46c64fff1b9f297ee57829d078aefccfe
Olivier <olivier_2000@live.nl>
2018-04-29 21:41:15 +0200

    Add not implemented classes

---
+++ Tests/GameTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    public class GameTests
+    {
+        private void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            Initialize();
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Tests/SessionTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class SessionTests
+    {
+        private void Initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
